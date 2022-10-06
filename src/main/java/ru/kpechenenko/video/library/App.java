68bf4f34a1c8e2a1521135cda8933674d622d738
Kostya Pechenenko <kpechenenko@gmail.com>
2022-10-07 01:07:07 +0700

    chore: add a demonstration

--- src/main/java/ru/kpechenenko/video/library/App.java
+++ src/main/java/ru/kpechenenko/video/library/App.java
@@ -1,11 +1,13 @@
 package ru.kpechenenko.video.library;
 
-public final class App {
-    public static String getGreeting() {
-        return "Hello World!";
-    }
+import ru.kpechenenko.video.library.service.VideoLibraryService;
 
+public final class App {
     public static void main(String[] args) {
-        System.out.println(getGreeting());
+        var videoLibraryService = new VideoLibraryService();
+        var movies = videoLibraryService.findAllMoviesReleasedInLastNYears(30);
+        for (var movie : movies) {
+            System.out.println(movie);
+        }
     }
 }
