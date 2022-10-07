8500b95a011266044816869d7893fca81683cd9a
Kostya Pechenenko <kpechenenko@gmail.com>
2022-10-08 04:44:46 +0700

    refactor: replace interface to implementation

--- src/main/java/ru/kpechenenko/video/library/App.java
+++ src/main/java/ru/kpechenenko/video/library/App.java
@@ -1,10 +1,10 @@
 package ru.kpechenenko.video.library;
 
-import ru.kpechenenko.video.library.service.VideoLibraryService;
+import ru.kpechenenko.video.library.service.VideoLibraryServiceImpl;
 
 public final class App {
     public static void main(String[] args) {
-        var videoLibraryService = new VideoLibraryService(new DataSource());
+        var videoLibraryService = new VideoLibraryServiceImpl(new DataSource());
         var movies = videoLibraryService.findAllMoviesReleasedInLastNYears(30);
         for (var movie : movies) {
             System.out.println(movie);
